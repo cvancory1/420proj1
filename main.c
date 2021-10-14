9d05d86c22b0a6988e2882392ca5fb3a1d4a10bd
cvancory1 <cvancory1@gmail.com>
2021-10-14 10:50:45 -0400

    a

--- main.c
+++ main.c
@@ -35,12 +35,12 @@ Chloe VanCory and Kalyn Howes
 //   return buf;
 // }
 
-  // typedef struct Users {
-  //   char *  username;
-  //   char *  id;
-  //   char *  alg;
-  //   char *  pwd;
-  // } Users;
+  typedef struct Users {
+    char *  username;
+    char *  id;
+    char *  alg;
+    char *  pwd;
+  } Users;
 
   int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -52,45 +52,73 @@ Chloe VanCory and Kalyn Howes
     MPI_Comm_size(world, &worldSize);
     MPI_Comm_rank(world, &rank);
     MPI_Get_processor_name(name, &nameLen);
-    // MPI_File shadowfh, wordsfh;
-    // MPI_Errhandler errorCheck;  // used to ensure the file has opened correctly
-
-
 
     /*
         read and parse the shadow txt file to get the username,id,salt, password
 
     */
-    // FILE * shadowPtr;
-    // shadowPtr = fopen ("shadow.txt", "r");
-    // char * line;
 
-    // Users shadowUsers; // TODO ask abotu this 
+  //  malloc the actual pointers then malloc each of hte arrays 
+    FILE * shadowPtr;
+    shadowPtr = fopen ("shadow.txt", "r");
+    char * line= malloc(255* sizeof(char));
+    int numUsers = 11;
+    Users shadowUsers[numUsers]; // 48 bytes 
+    int index = 0;
+ 
+      // printf("here=\n");
+    while(fscanf(shadowPtr,"%s", line )!=EOF){
+      // printf("here1=\n");
 
-    // while(fscanf(shadowPtr,"%s", line )!=EOF){
-      // char * username = strtok(line, ":" );
-      // char * alg = strtok(NULL, "$" );
-      // char * id = strtok(NULL, "$" );
-      // char * pwd = strtok(NULL, "$" );
+      // printf("line=%s\n",line);
+      char * username = strtok(line, ":" );
+      char * alg = strtok(NULL, "$" );
+      char * id = strtok(NULL, "$" );
+      char * pwd = strtok(NULL, "$" );
 
-      // shadowUsers[0].username = username;
-      // shadowUsers.alg = alg;
-      // shadowUsers.id = id;
-      // shadowUsers.pwd = pwd;
+      shadowUsers[index].username = username;
+      shadowUsers[index].alg = alg;
+      shadowUsers[index].id = id;
+      shadowUsers[index].pwd = pwd;
+      printf("i= %d username=%s\n",index, shadowUsers[index].username);
+      printf("alg=%s\n", shadowUsers[index].alg);
+      printf("id=%s\n", shadowUsers[index].id);
+      printf("pwd=%s\n\n", shadowUsers[index].pwd);
+
+      index++;
       
-      // printf("token=%s\n",username);
+      // printf("username=%s\n",username);
       // printf("alg=%s\n",alg);
       // printf("id=%s\n",id);
-      // printf("id=%s\n",pwd);
+      // printf("pwd=%s\n\n",pwd);
 
 
-    // }
-    // fclose(shadowPtr);
+    }
+    fclose(shadowPtr);
+
+    if(rank ==ROOT){
+
+
+    
+    // ERROR CHECKING 
+    for(int i =0; i < numUsers ;i++){
+    puts("-------");
+
+        printf("i= %d username=%s\n",i, shadowUsers[i].username);
+        printf("alg=%s\n", shadowUsers[i].alg);
+        printf("id=%s\n", shadowUsers[i].id);
+        printf("pwd=%s\n\n", shadowUsers[i].pwd);
+
+      }
+
+    }
 
 
     /*  
       READING THE WORDS.TXT file 
     */
+
+
     int * sendcnt;
     if(rank==ROOT){
      sendcnt = malloc(worldSize * sizeof(int));
@@ -136,9 +164,7 @@ Chloe VanCory and Kalyn Howes
 
       }else{
         printf("Error opening the testwords.txt\n");
-
       }
-
     }
 
 
@@ -154,33 +180,59 @@ Chloe VanCory and Kalyn Howes
   //  printf("rank = %d sendcnt= %d\n",rank ,sendcnt[rank]);
 
 
-   /*
-       use lseek to position file pointers and then read into then in a portion of the words.txt into a local dictionary 
-   */ 
 
+
+  /*
+      calcs the displacement for each proccessor to lseek ( move file pointer ) to a specific place
+      in the file
+  */
     int * displc = malloc(worldSize * sizeof(int));
     displc[0] = 0;
     for(int i =1; i < worldSize ; i++){
-      displc[i] = sendcnt[i-1] +displc[i-1];
+      displc[i] = sendcnt[i-1] + displc[i-1];
       if(rank ==0)
         printf("i =%d displc= %d\n",i, displc[i]);
 
     }
 
-    char * localDict = malloc ( sendcnt[rank] * sizeof(char));
-    lseek(fd ,displc[rank], SEEK_SET);
-    read(fd , localDict ,sendcnt[rank] );
 
-    // if(rank == 4){
-    //   printf("rank = %d \nstring= %s END\n",rank ,localDict);
-    //   // TODO: why is it reading in like this at the ery end 
+
+   /*
+       use lseek to position file pointers and then read into then in a portion of the words.txt into a local dictionary 
+   */ 
+    char * localDict = malloc ( sendcnt[rank]+1 * sizeof(char));
+    lseek(fd ,displc[rank], SEEK_SET);
+    int numRead = read(fd , localDict ,sendcnt[rank] );
+    localDict[strlen(localDict)]= 0; // places the NULL term @ the end
+
+    // if(rank == 3){
+    //   printf("rank = %d \nstring= %sEND\n",rank ,localDict);
+    // printf("Rank = %d numRead = %d   MALLOC  = %d  strlen(localDict)  =%lu\n",rank, numRead ,sendcnt[rank] , strlen(localDict ) );
+
     // }
+
+    if(rank == ROOT){
+        for(int i =0; i < worldSize ;i++){
+      // printf("rank = %d \nstring= %c%c%c END\n",rank ,localDict[displc[i-1]], localDict[displc[i]] , localDict[displc[i+1]]);
+        }
+      
+    }
+   
 
 
     
   /*
     Parse every words from every nodes local dictionary to crpyt and test ater 
   */
+
+  // while()
+
+
+
+// after every few itertations do an all reduce 
+ 
+
+
 
 
     MPI_Finalize();
