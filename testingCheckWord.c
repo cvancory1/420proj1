c4f88c76a5e3b83a2c320cb8a949cb0ff21476a8
howeskalyn <kalynhowes@gmail.com>
2021-10-13 22:12:04 -0400

    .

--- testingCheckWord.c
+++ testingCheckWord.c
@@ -215,7 +215,36 @@ int tensCheckPrefix(char *password, char *word) {
   return 0;
 }
 
+// checks password to dictionary word with prefixes 100-999
+int hundsCheckPrefix(char *password, char *word) {
+  int wordlen = strlen(word); // store dict word length
+  int buflen = (2 + wordlen * sizeof(char)); // buffer length
+  char *buf = malloc(buflen); // buffer to hold the dictionary word and the prefix/suffix
+  sprintf(buf, "%s", word); // puts null terminator after inserting word
 
+  char *prefix = malloc(3 * sizeof(char)); // three char spots
+  prefix[0] = '0';
+  prefix[1] = '0';
+  prefix[2] = '0';
+  char *prefixword = strcat(prefix, buf);
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      for (int k = 0; k < 10; k++) {
+      printf("(ones) prefixword: %s\n", prefixword);
+      char *crypted = crypting(prefixword); // crypt
+      int same = compare(password, crypted, prefixword); // compare
+      if (same == 1) {
+        return 1; 
+      }
+      prefixword[2]++;
+      }
+      prefixword[1]++;
+    }
+    prefixword[0]++;
+  }
+  return 0;
+}
 
 // checks dictionary word for each prefix & suffix size
 int checkWord(char *password , char *word) {
@@ -223,6 +252,8 @@ int checkWord(char *password , char *word) {
   if (onesCheckPrefix(password, word) == 1)
     return 1;
   if (tensCheckPrefix(password, word) == 1)
+    return 1;
+  if(hundsCheckPrefix(password, word) == 1)
     return 1;
 
   /*
@@ -240,7 +271,7 @@ int checkWord(char *password , char *word) {
 }
 
 int main () {
-  char *word = "abase";
+  char *word = "abandon";
   //char *testing = "aardvark123";
 
   // suffix checking
@@ -251,7 +282,8 @@ int main () {
 
   // prefix checking
   //char *password = "$1$ab$hNU8w62rGJrVyGwCWeCXq/"; // 4abashed
-  char *password = "$1$ab$AVH5ZeCRu0OFiaX7eM.2a0"; // 34abase
+  //char *password = "$1$ab$AVH5ZeCRu0OFiaX7eM.2a0"; // 34abase
+  char *password = "$1$abjRgy.EBTO7jyNsYPC5xOs0"; // 529abandon
 
   checkWord(password, word);
 
