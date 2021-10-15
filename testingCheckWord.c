5f0ec7befa96f184190f55d72351780d03d72f44
cvancory1 <cvancory1@gmail.com>
2021-10-14 22:16:41 -0400

    a

--- testingCheckWord.c
+++ testingCheckWord.c
@@ -168,9 +168,9 @@ int thousandsCheck (char *password, char *word) {
 // use strcat to add orefix string to the word buf
 int onesCheckPrefix(char *password, char *word) {
   printf("here in funcion ");
-  int wordlen = strlen(word); // store dict word length
+  // int wordlen = strlen(word); // store dict word length
   // int buflen = (2 + wordlen * sizeof(char)); // buffer length
-  printf(" wordlen %d \n", wordlen);
+  printf(" wordlen %d \n", strlen(word));
 
   // char *buf = malloc(2 + wordlen * sizeof(char)); // buffer to hold the dictionary word and the prefix/suffix
   
