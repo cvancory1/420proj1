23b824581d81d557a3dd1c4327626a9716f25878
cvancory1 <cvancory1@gmail.com>
2021-10-15 08:23:43 -0400

    a

--- main.c
+++ main.c
@@ -269,20 +269,22 @@ Chloe VanCory and Kalyn Howes
     fscanf(shadowPtr,"%s", line );
     char * username = strtok(line, ":" );
     char * alg = strtok(NULL, "$" );
-    char * salt = strtok(NULL, "$" );
-    char * pwd = strtok(NULL, "$" );
+    char * salt = strtok(NULL, "\n" );
+    // char * pwd = strtok(NULL, "$" );
+      printf("salt=%d", salt);
+
 
 
 
   // for(int i =0 ;i<numUsers ;i++){
   //   /* ALL NODES - parse their first word in the localdict */ 
-      int check;
-      char * currentWord = strtok(localDict, "\n" );
-      // if(rank ==ROOT) printf("rank = %d currentWord = %s pass=%s  pswdIndex =%d \n", rank, currentWord, pwd, pswdIndex);
+      // int check;
+      // char * currentWord = strtok(localDict, "\n" );
+      // // if(rank ==ROOT) printf("rank = %d currentWord = %s pass=%s  pswdIndex =%d \n", rank, currentWord, pwd, pswdIndex);
 
-      // if(rank ==ROOT){
-      check = checkWord(strcat(salt,pwd) , currentWord);
-      // printf("IN MAIN");
+      // // if(rank ==ROOT){
+      // check = checkWord(pwd , currentWord);
+      // // printf("IN MAIN");
 
       // }
       // printf("RANK = %d BACK IN MAIN\n", rank);
