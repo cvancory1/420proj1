4ee2c492bc319e813fb8a36fdd12dca9edc22fd5
cvancory1 <cvancory1@gmail.com>
2021-10-14 22:03:43 -0400

    a

--- testingCheckWord.c
+++ testingCheckWord.c
@@ -172,7 +172,7 @@ int onesCheckPrefix(char *password, char *word) {
   char *buf = malloc(buflen); // buffer to hold the dictionary word and the prefix/suffix
   sprintf(buf, "%s", word); // puts null terminator after inserting word
 
-  // char *prefix = malloc(2 * sizeof(char));  // one larger than prefix size for null
+  char *prefix = malloc(2 * sizeof(char));  // one larger than prefix size for null
   // prefix[0] = '0';
   // prefix[1] = '\0';
   // char *prefixword = strcat(prefix, buf);
