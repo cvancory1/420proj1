997a476cab1a6f65f69885af96393e6081b6dfb4
cvancory1 <cvancory1@gmail.com>
2021-10-14 22:52:20 -0400

    a

--- main.c
+++ main.c
@@ -300,10 +300,8 @@ Chloe VanCory and Kalyn Howes
         currentWord = strtok(NULL, "\n" );
         if(currentWord !=NULL){
           check = checkWord(currentWord , pwd);
-
-
         }
-          // printf("rank = %d currentWord = %s\n", rank, currentWord);
+          printf("rank = %d currentWord = %s\n", rank, currentWord);
 
         // if( check == 1 ){
         //   usrPwd[pswdIndex]= 1; // this node found a password
