5ef5e4a6ef63ed708e847cdf26d4c5c05f914a01
cvancory1 <cvancory1@gmail.com>
2021-10-16 00:00:41 -0400

    adding project slurm

--- myproject.sh
+++ myproject.sh
@@ -1,12 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
-File: myproject.sh
+# File: myproject.sh
 
-SBATCH --job-name=myproject  # a logical name of your choice
-SBATCH --ntasks=30           # like doing -n 30
-SBATCH --mem=4gb             # Each task gets this much memory (if available)
-SBATCH --time=00:15:00       # Time limit in the form hh:mm:ss
-SBATCH --output=out/%j.log   # any stdout will be redirected to the file out/[JOB ID].log
+#SBATCH --job-name=myproject  # a logical name of your choice
+#SBATCH --ntasks=30           # like doing -n 30
+#SBATCH --mem=2gb             # Each task gets this much memory (if available)
+#SBATCH --time=00:05:00       # Time limit in the form hh:mm:ss
+#SBATCH --output=out/%j.log   # any stdout will be redirected to the file out/[JOB ID].log
 
 module load mpi/mpich-3.2-x86_64
 
@@ -14,4 +14,4 @@ module load mpi/mpich-3.2-x86_64
 # mpirun python3 ~/Projects/myproject/myproject.py
 
 # For a C MPI project
-# mpirun ~/Projects/myproject/a.out 
+mpirun ~/Projects/myproject/a.out 
